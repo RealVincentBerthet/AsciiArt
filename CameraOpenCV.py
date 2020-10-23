1be78ca51511c70a992a5a0ff0192728e96defc1
Vincent Berthet <vincent.berthet42@gmail.com>
2020-10-23 19:07:23 +0200

    add video support

---
+++ CameraOpenCV.py
@@ -0,0 +1,154 @@
+import cv2 as cv
+import numpy as np
+import glob
+from datetime import datetime
+
+class Camera:
+    def __init__(self,camera_stream=0,calibration_file=None):
+        # Create a VideoCapture object
+        if isinstance(camera_stream, int):
+            #Capture from device camera
+            print('index')
+            self.CAP=cv.VideoCapture(camera_stream,cv.CAP_DSHOW)
+        else : 
+            # Capture from loaded video
+
+            self.CAP=cv.VideoCapture(str(camera_stream))
+
+        # Set calibration if it has been saved
+        if not calibration_file==None :
+            with np.load(str(calibration_file)) as X:
+                self.mtx, self.dist, _, _ = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
+
+    def __del__(self):
+        self.CAP.release()
+
+    def getFrame(self,loop=False):
+        frame=None
+        # Capture frame-by-frame
+        if self.CAP.isOpened() :
+            ret, frame = self.CAP.read()
+            if not ret and loop:
+                    # End of video reached reset to the first frame
+                    self.CAP.set(cv.CAP_PROP_POS_FRAMES, 0)
+                    return self.getFrame()
+        else:
+            print('Unable to read camera feed. ')
+
+        return frame
+
+    def getCap(self):
+        return self.CAP
+
+    def getCapFps(self):
+            return self.CAP.get(cv.CAP_PROP_FPS)
+
+    def getNbFrames(self):
+        return int(self.CAP.get(cv.CAP_PROP_FRAME_COUNT))
+        
+    @staticmethod    
+    def recordStream(stream,output='datetime.mp4',fps=10):
+        if len(stream) > 0 :
+            if output=='datetime.mp4':
+                output=datetime.now().strftime('%Y-%m-%d_%H%M%S')+'.mp4'
+            # Define the codec and create VideoWriter object
+            height, width, channels = stream[0].shape
+            out = cv.VideoWriter(output,cv.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
+            for frame in stream:
+                out.write(frame)
+            out.release()
+            print('[INFO] Record of '+str(len(stream))+' frames successful : '+str(output))    
+        else : 
+            print('[ERROR] Stream is empty')
+
+    @staticmethod
+    def createCalibrationFile(jpeg_directory,output='calibration.npz',rows=7,cols=6):
+        """
+        :param rows: Define the chess board rows 
+        :param cols: Define the chess board columns 
+        """
+        # Set the termination criteria for the corner sub-pixel algorithm
+        criteria = (cv.TERM_CRITERIA_MAX_ITER + cv.TERM_CRITERIA_EPS, 30, 0.001)
+        # Prepare the object points: (0,0,0), (1,0,0), (2,0,0), ..., (6,5,0). They are the same for all images
+        objp = np.zeros((rows * cols, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:rows, 0:cols].T.reshape(-1, 2)
+        # Create the arrays to store the object points and the image points
+        objpoints = []
+        imgpoints = []
+        images = glob.glob(str(jpeg_directory)+'/*.jpg')
+        for fname in images:
+            # Load the image and convert it to gray scale
+            img = cv.imread(fname)
+            img = cv.resize(img,(640,480))
+            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+            # Find the chess board corners
+            ret, corners = cv.findChessboardCorners(gray, (rows,cols), None)
+            # If found, add object points, image points (after refining them)
+            if ret == True:
+                objpoints.append(objp)
+                corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+                imgpoints.append(corners)
+                # Draw and display the corners
+                cv.drawChessboardCorners(img, (rows,cols), corners2, ret)
+                cv.imshow('chess', img)
+                cv.waitKey(500)
+        cv.destroyAllWindows()
+
+        # Calibrate the camera and save the results
+        ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+        np.savez(str(output), mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+        error = 0
+
+        for i in range(len(objpoints)):
+            point, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+            error += cv.norm(imgpoints[i], point, cv.NORM_L2) / len(point)
+
+        print("Total error: ", error / len(objpoints))
+
+        # Load one of the test images
+        img2 = cv.imread(images[0])
+        img2 = cv.resize(img2,(img.shape[1],img.shape[0]))
+        h, w = img.shape[:2]
+
+        # Obtain the new camera matrix and undistort the image
+        newCameraMtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+        undistortedImg = cv.undistort(img2, mtx, dist, None, newCameraMtx)
+        # x, y, w, h = roi
+        # undistortedImg = undistortedImg[y:y+h, x:x+w]
+        # cv.imwrite('calibresult.png', undistortedImg)
+
+        # Display the final result
+        cv.imshow('chess', np.hstack((img2, undistortedImg)))
+        cv.waitKey(0)
+        print('Calibration successful')
+
+    def reconstruction(self):
+        pass
+
+    @staticmethod
+    def cutVideo():
+        pass   
+
+
+
+def testRecord():
+    camera=Camera('sample.mp4') 
+    stream=[]
+    while True:
+        frame=camera.getFrame()
+
+        if frame.all() :
+            break
+        
+        stream.append(frame)
+        cv.imshow('[testRecord] stream',frame)
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            cv.destroyAllWindows()
+            break
+
+    Camera.recordStream(stream,fps=camera.getCapFps())
+
+# TESTS
+# testRecord()
--- asciiArt.py
+++ asciiArt.py
@@ -12,6 +12,7 @@ import argparse
 import os
 from PIL import Image, ImageFont, ImageDraw, ImageOps
 from datetime import datetime
+from CameraOpenCV import Camera
 
 def get_args():
     """
@@ -20,20 +21,34 @@ def get_args():
     :return: argurments set by default or overriden
     """
     parser = argparse.ArgumentParser(description='Process an image to create ASCII Art. Output can be a text file [.txt] or an image [.jpg, .png]. You can tune the different optionnal parameters to change the result. More details in the dedicated README.')
-    parser.add_argument('-i','--input', type=str, default='sample.jpg', help='Path to the input image')
-    parser.add_argument('-o','--output', type=str, default=' .jpg', help='Path to the output file')
+    parser.add_argument('-i','--input', type=str, default='sample.jpg', help='Path to the input image (can be a video or you camera)')
+    parser.add_argument('-o','--output', type=str, default='', help='Path to the output file')
     parser.add_argument('-r','--rows', type=int, default=256, help='Number of ASCII rows')
     parser.add_argument('-c','--columns', type=int, default=256, help='Number of ASCII columns')
     parser.add_argument('-d','--dictionnary', type=str, default='simple', choices=['simple', 'complex', 'vberthet'], help='Choose a dictionnary to use')
     parser.add_argument('-f','--font', type=str, default='fonts/deja-vu/DejaVuSansMono-Bold.ttf', choices=['fonts/deja-vu/DejaVuSansMono-Bold.ttf','fonts/caviar-dreams/CaviarDreams_Bold.ttf'], help='[IMG only] Font to use')
     parser.add_argument('-fs','--fontSize',type=int,default=10,help='[IMG only] Size of the font')
     parser.add_argument('-bg','--background', type=int, default=255, help='[IMG only] Background color')
-    parser.add_argument('--debug', default=False, action='store_true')
+    parser.add_argument('--debug', default=False, action='store_true', help='Enable debug and show results')
+    parser.add_argument('--txt', default=False, action='store_true', help='Force output to .txt file')
+    parser.add_argument('--unsave', default=False, action='store_true', help='Force to unsave the output file')
 
     args = parser.parse_args()
     return args
 
-def toTxt(img,dictionnary,path):
+def preProcess(src):
+    # Pre-process input image
+    if(isinstance(src, str)):
+        img = cv.imread(src)
+    else :
+        img=src
+
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.resize(img,(opt.columns,int(opt.rows/2)),interpolation=cv.INTER_CUBIC)
+
+    return img
+
+def toTxt(src,dictionnary,path=None):
     """
     toTxt function process an image to create an ASCII Art text file
 
@@ -41,21 +56,23 @@ def toTxt(img,dictionnary,path):
     :param dictionnary: ASCII char list used to encode pixels value
     :param path: path to the output file
     """
-    output_file = open(path, 'w')
+    img=preProcess(src)
+    text=''
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            index=int(img[i,j]*len(dictionnary)/255)
-            output_file.write(dictionnary[index])
-        output_file.write("\n")
-    output_file.close()
+            index=int(img[i,j]*len(dictionnary)/256)
+            text+=dictionnary[index]
+        text+='\n'
+    
+    if not opt.unsave and not path==None:
+        output_file = open(path, 'w')
+        output_file.write(text)
+        output_file.close()
+        print('[TXT]',path,'saved')
 
-    if opt.debug:
-        with open(path, 'r') as f:
-            print(f.read())
+    return text
 
-    print('[TXT]',path,'saved')
-
-def toImg(img,dictionnary,path):
+def toImg(src,dictionnary,path=None):
     """
     toImg function process an image to create an ASCII Art image file
 
@@ -63,6 +80,8 @@ def toImg(img,dictionnary,path):
     :param dictionnary: ASCII char list used to encode pixels value
     :param path: path to the output file
     """
+    img=preProcess(src)
+
     font = ImageFont.truetype(opt.font, size=opt.fontSize)
     font_width, font_height = font.getsize('X')
     out_width = font_width * opt.columns
@@ -72,14 +91,14 @@ def toImg(img,dictionnary,path):
     
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            index=int(img[i,j]*len(dictionnary)/255)
+            index=int(img[i,j]*len(dictionnary)/256)
             draw.text((j*font_width, i * font_height), dictionnary[index], fill=255 - opt.background, font=font)
 
-    if opt.debug :
-        output_file.show()
+    if not opt.unsave and not path==None :
+        output_file.save(path)
+        print('[IMG]',path,'saved')
 
-    output_file.save(path)
-    print('[IMG]',path,'saved')
+    return output_file
 
 def main(opt):
     # Choose dictionnary
@@ -91,24 +110,39 @@ def main(opt):
         dictionnary = '@%#*+=-:. '
         
     # Set output
+    (ifile, iext) = os.path.splitext(os.path.basename(opt.input))
     (file, ext) = os.path.splitext(os.path.basename(opt.output))
-    if file=='' or file==' '  : 
-        path='output/'+os.path.splitext(os.path.basename(opt.input))[0]+'-'+datetime.now().strftime("%Y-%m-%d_%H%M%S")+ext
+    if file=='' or ext=='' : 
+        ext='.txt' if ext=='' and opt.txt else '.jpg'
+        path='output/'+os.path.splitext(os.path.basename(opt.input))[0]+'-'+datetime.now().strftime('%Y-%m-%d_%H%M%S')+ext
     else:
         path=opt.output
 
     if not os.path.dirname(path)=='' and not os.path.exists(os.path.dirname(path)):
         os.makedirs(os.path.dirname(path))
         
-    # Pre-process input image
-    src = cv.imread(opt.input)
-    src = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-    src = cv.resize(src,(opt.columns,int(opt.rows/2)),interpolation=cv.INTER_CUBIC)
-
-    if ext=='.txt':
-        toTxt(src,dictionnary,path)
+    if iext=='.jpg' or iext=='.png' or iext=='.jpeg':
+        if opt.txt:
+            art=toTxt(opt.input,dictionnary,path)
+            if opt.debug :
+                print(art)
+        else:
+            art=toImg(opt.input,dictionnary,path)
+            if opt.debug :
+                art.show()
     else:
-        toImg(src,dictionnary,path)
+        print('[INFO] Use video as the input (video file or streaming from camera)')
+        print('[INFO] To help realtime conversion : only image to .txt will be done, save output is disabled')
+        camera=Camera(opt.input)
+        while True:
+            frame=camera.getFrame()
+            if frame.all() :
+                break
+            else:
+                cv.imshow('Source',frame)
+                cv.waitKey(1)
+                art=toTxt(frame,dictionnary)
+                print(art)
 
 if __name__ == '__main__':
     opt = get_args()
